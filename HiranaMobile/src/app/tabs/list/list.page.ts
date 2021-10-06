dff1d1ef4bb2ffd9e938c392c925fbd4039ab80b
Alexander Eberle <alexander171294@gmail.com>
2021-10-06 15:41:22 -0300

    details

--- HiranaMobile/src/app/tabs/list/list.page.ts
+++ HiranaMobile/src/app/tabs/list/list.page.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
+  public loading = true;
+  public channels;
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+  }
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
   }
 
 }
